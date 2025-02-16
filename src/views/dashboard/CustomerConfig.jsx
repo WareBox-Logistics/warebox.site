22ed793eabab1cb674912b91527f8b83a506e68d
AlexisPN <0321101363@ut-tijuana.edu.mx>
2025-02-15 19:09:21 -0800

    screen employe

--- src/views/dashboard/CustomerConfig.jsx
+++ src/views/dashboard/CustomerConfig.jsx
@@ -8,8 +8,8 @@ import { Button } from 'antd';
 const CustomerConfig = () => {
   return (
     <Paper elevation={3} sx={{ padding: '16px', margin: '16px' }}>
-      <MainCard title="Sample Card">
-        <Typography variant="body2">
+      {/* <MainCard title="Sample Card"> */}
+        {/* <Typography variant="body2">
           Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion
           tempos incident ut laborers et doolie magna alissa. Ut enif ad minim
           venice, quin nostrum exercitation illampu laborings nisi ut liquid ex
@@ -17,9 +17,10 @@ const CustomerConfig = () => {
           veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean
           cuspidate non president, sunk in culpa qui officiate descent molls
           anim id est labours.
-        </Typography>
-        <Button type="primary">Primary Button</Button>
-      </MainCard>
+        </Typography> */}
+      {/* </MainCard> */}
+      <Button type="primary">Primary Button</Button>
+
     </Paper>
   );
 };
