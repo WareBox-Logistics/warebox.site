056d24f7fc4d51405677ca01ab78ae13ef60c209
thebancer <0321101710@ut-tijuana.edu.mx>
2025-03-19 20:04:56 -0700

    dashboardis report

--- src/views/pages/despacho/DashboardDis.jsx
+++ src/views/pages/despacho/DashboardDis.jsx
@@ -1,13 +1,19 @@
-import {
-    Paper,
-    Typography,
-  } from "@mui/material";
-  import MainCard from "ui-component/cards/MainCard";
-  
+import React, { useEffect, useState, useRef } from 'react';
+import { Paper } from "@mui/material";
+import { Card, Col, Row, Input, Button, Table, message, Select, Spin, Modal, Typography } from "antd";
+import { UserAddOutlined, SearchOutlined, LoadingOutlined, EditOutlined, DeleteOutlined, CloseOutlined } from "@ant-design/icons";
+import MainCard from "ui-component/cards/MainCard";
+import axios from 'axios';
+import { authToken, API_URL_COMPANY, API_URL_SERVICE } from '../../../services/services';
+const { Text } = Typography;
+
+
+
   const DashboardDis = () => {
+
     return (
       <Paper elevation={3} sx={{ padding: '16px', margin: '16px' }}>
-        <MainCard title="Sample Card">
+        <MainCard title="Dashboard">
           <Typography variant="body2">
             Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion
             tempos incident ut laborers et doolie magna alissa. Ut enif ad minim
