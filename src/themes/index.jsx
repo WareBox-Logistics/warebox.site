658235906ec40aa5806787af4481472cee61d4ff
hectoruwuxd <zamorano0903@gmail.com>
2025-04-02 14:47:42 -0700

    Colors theme updated

--- src/themes/index.jsx
+++ src/themes/index.jsx
@@ -25,7 +25,7 @@ export const theme = (customization) => {
     darkTextPrimary: color.grey700,
     darkTextSecondary: color.grey500,
     textDark: color.grey900,
-    menuSelected: color.secondaryDark,
+    menuSelected: color.secondary800,
     menuSelectedBack: color.secondaryLight,
     divider: color.grey200,
     customization
